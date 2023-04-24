5056d7f3e829c72dda89db1242e0ca3502763c52
Obadias Pelembe <jr.pelembeobadias@gmail.com>
2023-04-25 00:40:35 +0200

    fix: remove root command

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,6 @@ package cmd
 import (
 	"fmt"
 	"os"
-
-	"linsync/helper"
-
 	"github.com/spf13/cobra"
 )
 
@@ -17,30 +14,6 @@ var rootCommand = &cobra.Command{
 	Version: version,
 	Short:   SHORT_DESCRIPTION,
 	Long:    SHORT_DESCRIPTION,
-
-	Run: func(cmd *cobra.Command, args []string) {
-
-		if len(args) >= 2 {
-			var subCommands = args[0]
-			var filename = args[1]
-
-			switch subCommands {
-			case "push":
-				helper.PushFile(filename)
-				break
-			case "pull":
-				helper.PullFile(filename)
-				break
-			default:
-				fmt.Println("unknown command")
-			}
-
-			return
-		}
-
-		fmt.Println(SHORT_DESCRIPTION)
-
-	},
 }
 
 func Execute() { 
